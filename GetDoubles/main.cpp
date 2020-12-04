2dda332395d43dbd1159531db084442c23287cb5
fjmurray <fjmurray@live.com>
2020-12-04 14:34:19 -0500

    Initial Commit

---
+++ GetDoubles/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  GetDoubles
+//
+//  Created by Frank Murray on 04/ 12/20.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
