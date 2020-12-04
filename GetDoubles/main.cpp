0c8e0c554cca6e0d946f9c0a74ffcdcadde6f3c7
fjmurray <fjmurray@live.com>
2020-12-04 14:44:26 -0500

    C++ program showing integrated use of dynamic array(s) and pointer(s),,
    File Name:   GetDoubles.cpp
    
    updated:   Friday, 04 Dec 2020. 14:44;

--- GetDoubles/main.cpp
+++ GetDoubles/main.cpp
@@ -6,9 +6,49 @@
 //
 
 #include <iostream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
+double *getDoubles (int numDoubles);
+
+int main(int argc, const char * argv[])
+{
     // insert code here...
-    std::cout << "Hello, World!\n";
+    
+    int count = 0, number = 0;
+    double *doublesArray = {0};
+    
+    cout << "\nInput the size (positive integer) of the array: ";
+    cin >> number;
+    
+    doublesArray = getDoubles (number);
+    
+    for (count = 0; count < number; count++)
+    {
+        cout << doublesArray[count] << " ";
+    }
+    
+    delete doublesArray;
+    
+    cout << "\n\nHello, Solar System!\n";
+    
     return 0;
+}
+
+double *getDoubles (int numDoubles)
+{
+    int count = 1, index = 0;
+    double *myDoubles;
+    double three = 3;
+    
+    myDoubles = new double[numDoubles];
+    
+    for (index = 0; index < numDoubles; index++)
+    {
+        myDoubles[index] = (double) (count / three);
+        count++;
+    }
+    
+    return myDoubles;
+    
+    // delete myDoubles;   <--- code never gets executed, just fyi;
 }
